de6979b3a183ed690a28d6c713e58c60430149bf
XxinYii <137269390+XxinYii@users.noreply.github.com>
2023-12-27 15:55:17 +0800

    level 4 done

--- G4FinalProject_DMT/Assets/Scripts/L4-Script/Sticky Platform.cs
+++ G4FinalProject_DMT/Assets/Scripts/L4-Script/Sticky Platform.cs
@@ -6,16 +6,24 @@ public class StickyPlatform : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Santa" )
+        if (collision.gameObject.name == "Santa")
         {
-            collision.gameObject.transform.SetParent(transform);
+            StartCoroutine(SetParentCoroutine(collision.gameObject.transform, transform));
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Santa") { 
-            collision.gameObject.transform.SetParent(null);
+        if (collision.gameObject.name == "Santa")
+        {
+            StartCoroutine(SetParentCoroutine(collision.gameObject.transform, null));
         }
+    }
+
+    private IEnumerator SetParentCoroutine(Transform child, Transform parent)
+    {
+        yield return new WaitForEndOfFrame(); // Wait until the end of the frame
+
+        child.SetParent(parent);
     }
 }
