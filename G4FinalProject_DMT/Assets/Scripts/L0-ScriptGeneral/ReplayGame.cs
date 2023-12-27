40792b68908e56ed9a307278b87d431f050698f7
Qinweiiii <swe2109537@xmu.edu.my>
2023-12-28 01:04:37 +0800

    endgame scene byebye

---
+++ G4FinalProject_DMT/Assets/Scripts/L0-ScriptGeneral/ReplayGame.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReplayGame : MonoBehaviour
+{
+    public void Replay()
+    {
+        SceneManager.LoadScene("1-StartGame");
+    }
+}
